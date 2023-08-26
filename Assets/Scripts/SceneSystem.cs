ca488ca8cdfb6da853e9ba142163c6f06219dce3
KrsMaple <142078922+KrsMaple@users.noreply.github.com>
2023-08-26 16:11:13 +0800

    Update 場景程式

---
+++ Assets/Scripts/SceneSystem.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;		//unity內建場景管理方法
+
+public class SceneSystem : MonoBehaviour
+{
+	public void StartGame()
+	{
+		//場景管理_讀取場景("場景名稱")
+		SceneManager.LoadScene("遊戲場景");
+	}
+
+	public void QuitGame()
+	{
+		print("<i><b>離開</b>遊戲</i>");
+		//應用_退出
+		Application.Quit();
+	}
+}
